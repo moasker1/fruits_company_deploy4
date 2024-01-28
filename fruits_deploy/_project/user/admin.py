6eb1a80bcd2372b6d175c3548bc82fe8935e9141
askar <maeaxvx@gmail.com>
2024-01-28 19:29:30 +0200

    initialize fruits deploy 4 ba3gar

---
+++ fruits_deploy/_project/user/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Supplier, Seller, Container, Item, ContainerItem, Sale,Payment, Lose, ContainerExpense, ContainerBill, SupplierPay, RecentAction
+from django.contrib.auth.models import Group
+
+admin.site.site_header = 'Tiger tech'
+admin.site.site_title = 'إدارة موقع تايجر'
+admin.site.index_title = 'الإدارة'
+
+
+admin.site.unregister(Group)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+admin.site.register(Container)
+admin.site.register(Supplier)
+admin.site.register(Seller)
+admin.site.register(Item)
+admin.site.register(ContainerItem)
+admin.site.register(Sale)
+admin.site.register(Payment)
+admin.site.register(Lose)
+admin.site.register(ContainerExpense)
+admin.site.register(ContainerBill)
+admin.site.register(SupplierPay)
+admin.site.register(RecentAction)
